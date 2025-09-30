77692bd90cb1f7201160a26dd5c634aa94018a8d
dilaa2002 <148992798+dilaa2002@users.noreply.github.com>
2025-10-01 00:11:25 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-const express = require("express");
-const app = express();
-__path = process.cwd();
-const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 8000;
-let code = require("./pair");
-require("events").EventEmitter.defaultMaxListeners = 500;
-app.use("/code", code);
+// deploy repo is closed no longer available for any type of deployment 👀
 
-app.use("/", async (req, res, next) => {
-  res.sendFile(__path + "/pair.html");
-});
+// Deploy From New Repository ✅ 
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(PORT, () => {
-  console.log(`⏩ Server running on http://localhost:` + PORT);
-});
+// KHAN-MD version 5.0 ❤️
 
-module.exports = app;
+// Thanks For Chosing KHAN MD 🔥
+
+// Made with ♥️ By JawadTech 
+
+// Powered By JawadTechX KHAN-MD 2025 🚀
